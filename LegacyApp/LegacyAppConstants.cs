8390ed03efacde8337792494f0abaadf011cd2eb
Jakub Drożdż <s24871@pjwstk.edu.pl>
2024-03-24 15:30:01 +0100

    UserService tests implemented

---
+++ LegacyApp/LegacyAppConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace LegacyApp;
+
+public class LegacyAppConstants
+{
+    public static readonly string VERY_IMPORTANT_CLIENT = "VeryImportantClient";
+    public static readonly string IMPORTANT_CLIENT = "ImportantClient";
+}
